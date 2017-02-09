03bdd80d599ec2b1dcecef30cc5a59092f3b776c
Henri Nikka <henri@kiskolabs.com>
2017-02-09 12:54:30 +0200

    Add a frame container with max-width.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="frame">
+        <div className="title-bar">
+          Title
+        </div>
         <h1 className="size-1-5">Ripley prototype</h1>
+
+        <div className="tab-bar">
+          Nav
+        </div>
       </div>
     );
   }
