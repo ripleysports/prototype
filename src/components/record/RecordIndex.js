c3ee74c6c64f3a57778b482c72225ca8f2d9cd69
Henri Nikka <henri@kiskolabs.com>
2017-02-10 16:12:53 +0200

    Add some more dummy data to the json. Change recordIndex heading styles.

--- src/components/record/RecordIndex.js
+++ src/components/record/RecordIndex.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class RecordIndex extends Component {
   render() {
     return (
-      <h1> Nauhoitus </h1>
+      <div>
+        <h1 className="bold margin-1 margin-bottom size-1-5">Nauhoitus</h1>
+      </div>
     );
   }
 }
