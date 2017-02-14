2b3093b85a8f90f2d3d712b5f5958f6de86cb8da
Henri Nikka <henri@kiskolabs.com>
2017-02-14 10:33:02 +0200

    Add rink image to recordActive and game components.

--- src/components/games/Game.js
+++ src/components/games/Game.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import data from '../../data/data.json';
 import _ from 'lodash';
+import Isvg from 'react-inlinesvg';
+import rink from '../../images/rink.svg';
 
 class Game extends Component {
   render() {
     const game = _.find(data.games, (game) => game.slug === this.props.params.gameSlug);
     return (
       <div className="padding-1">
+        <Isvg src={rink} className=""> </Isvg>
         <ul>
           {_.keys(game).map(function (key) {
               return (
--- src/components/record/RecordActive.js
+++ src/components/record/RecordActive.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Isvg from 'react-inlinesvg';
+import rink from '../../images/rink.svg';
 
 class RecordTeams extends Component {
   render() {
     return (
       <div className="padding-1 text-align-center">
+        <Isvg src={rink} className=""> </Isvg>
         <h1 className="bold margin-1 margin-bottom size-1-5">Nauhoitus käynnissä</h1>
         <p className="margin-1 margin-bottom">Tallenna momentti</p>
         <Link to={`/record/ended`} className="color-primary">
