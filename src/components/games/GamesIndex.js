ced819888ea218a80e1d1d67113afa358671e989
Henri Nikka <henri@kiskolabs.com>
2017-02-14 10:53:55 +0200

    Get team names to games listing.

--- src/components/games/GamesIndex.js
+++ src/components/games/GamesIndex.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import _ from 'lodash';
 import data from '../../data/data.json';
 
 class GamesIndex extends Component {
+  renderGameList() {
+    return data.games.map(game => {
+      const homeTeam = _.find(data.teams, (team) => team.id === game.home);
+      const awayTeam = _.find(data.teams, (team) => team.id === game.away);
+      return (
+        <Link to={`/games/${game.slug}`} key={game.id} className="block padding-0-5 padding-y border-color-gray-lighten-3 color-primary">
+          <div className="padding-1 padding-x">{homeTeam.name} vs {awayTeam.name}</div>
+        </Link>
+      );
+      }
+    );
+  }
   render() {
     return (
       <div>
@@ -10,11 +23,7 @@ class GamesIndex extends Component {
           <h1 className="bold margin-1 margin-bottom size-1-5">Pelit</h1>
         </div>
         <ul className="child-borders-y">
-          {data.games.map(game =>
-            <Link to={`/games/${game.slug}`} key={game.id} className="block padding-0-5 padding-y border-color-gray-lighten-3 color-primary">
-              <div className="padding-1 padding-x">{game.name}</div>
-            </Link>)
-          }
+          {this.renderGameList()}
         </ul>
       </div>
     );
