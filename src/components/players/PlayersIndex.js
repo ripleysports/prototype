1f6528cf6413557156d7c0939711102aa9aaf61d
Henri Nikka <henri@kiskolabs.com>
2017-02-16 00:43:01 +0200

    Style playerIndex screen.

--- src/components/players/PlayersIndex.js
+++ src/components/players/PlayersIndex.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import _ from 'lodash';
 import data from '../../data/data.json';
+import Isvg from 'react-inlinesvg';
+import chevron from '../../images/icons/icon-chevron-r.svg';
 
 class PlayersIndex extends Component {
   render() {
     return (
       <div>
-        <div className="padding-1 padding-bottom-0">
-          <h1 className="bold margin-1 margin-bottom size-1-5">Pelaajat</h1>
+        <div className="padding-1 padding-top">
+          <ul className="child-borders-y">
+            {data.players.map(player =>
+              <Link to={`/players/${player.slug}`} key={player.id} className="block padding-0-5 padding-y border-color-primary-lighten-1">
+                <div className="flex align-center child-margins-x-1 padding-1 padding-x">
+                  <div className="width-2 height-2 round bg-secondary"></div>
+                  <div className="grow">{player.firstName} {player.lastName}</div>
+                  <Isvg src={chevron} className="stroke-primary-lighten-2 width-0-75"> </Isvg>
+                </div>
+              </Link>)
+            }
+          </ul>
         </div>
-        <ul className="child-borders-y">
-          {data.players.map(player =>
-            <Link to={`/players/${player.slug}`} key={player.id} className="block padding-0-5 padding-y border-color-gray-lighten-3 color-primary">
-              <div className="padding-1 padding-x">{player.firstName} {player.lastName}</div>
-            </Link>)
-          }
-        </ul>
       </div>
     );
   }
