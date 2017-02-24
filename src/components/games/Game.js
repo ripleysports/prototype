ba0d6d6750de1ef10f41131695bbb8e8cfecacb8
Henri Nikka <henri@kiskolabs.com>
2017-02-24 14:32:50 +0200

    Change game screen to match coach game screen.

--- src/components/games/Game.js
+++ src/components/games/Game.js
@@ -5,6 +5,7 @@ import Slider from 'rc-slider';
 import _ from 'lodash';
 import Isvg from 'react-inlinesvg';
 import rink from '../../images/rink.svg';
+import watch from '../../images/icons/icon-watch.svg';
 
 class Game extends Component {
   constructor(props) {
@@ -13,7 +14,8 @@ class Game extends Component {
       game: this.setGame(),
       teams: this.setTeams(),
       players: this.setPlayers(),
-      moments: this.setMoments()
+      moments: this.setMoments(),
+      momentSelected: false
     }
   }
   setGame() {
@@ -59,12 +61,53 @@ class Game extends Component {
           </Link>)
     });
   }
+  calculateMomentOffset(time) {
+    const splitTime = /^(\d+):(\d+)$/.exec(time);
+    const totalTime = (parseInt( splitTime[1]) * 60) + parseInt( splitTime[2] );
+    const totalTimeOffset = totalTime - 15;
+
+    let minutes = parseInt( (totalTimeOffset/ 60), 10 );
+    let seconds = totalTimeOffset % 60;
+    return `${("00" + minutes).slice(-2)}:${("00" + seconds).slice(-2)}`;
+  }
+  renderPlayers() {
+    return (
+      <div>
+        <div className="width-0-5 height-0-5 round absolute bg-green" style={{top: "50%", left: "50%"}}></div>
+        <div className="width-0-5 height-0-5 round absolute bg-green" style={{top: "10%", left: "30%"}}></div>
+        <div className="width-0-5 height-0-5 round absolute bg-green" style={{top: "30%", left: "40%"}}></div>
+        <div className="width-0-5 height-0-5 round absolute bg-green" style={{top: "80%", left: "28%"}}></div>
+        <div className="width-0-5 height-0-5 round absolute bg-green" style={{top: "50%", left: "65%"}}></div>
+
+        <div className="width-0-5 height-0-5 round absolute bg-red" style={{top: "20%", left: "50%"}}></div>
+        <div className="width-0-5 height-0-5 round absolute bg-red" style={{top: "10%", left: "70%"}}></div>
+        <div className="width-0-5 height-0-5 round absolute bg-red" style={{top: "90%", left: "60%"}}></div>
+        <div className="width-0-5 height-0-5 round absolute bg-red" style={{top: "60%", left: "40%"}}></div>
+        <div className="width-0-5 height-0-5 round absolute bg-red" style={{top: "15%", left: "30%"}}></div>
+      </div>
+    )
+  }
   renderMoments() {
     const moments = this.state.moments;
     return moments.map(moment => {
       return (
-          <li key={moment.id} className="padding-0-5 border-color-primary-lighten-1">
-          {moment.time}
+          <li key={moment.id} className="flex align-center child-margins-x-0-5 padding-0-5 border-color-primary-lighten-1 border-color-primary-lighten-1">
+            <div className="grow flex align-center child-margins-x-0-5 pointer">
+              <Isvg src={watch} className="fill-white width-1-5"> </Isvg>
+              <div className="grow">
+                <div className="size-0-75">
+                  Momentti {moment.id}
+                </div>
+                <div>
+                  {this.calculateMomentOffset(moment.time)}
+                  -
+                  {moment.time}
+                </div>
+              </div>
+            </div>
+            <Link to={`/coach/moment`} className="button button--small">
+              Kommentit (2)
+            </Link>
           </li>
         )
     });
@@ -84,19 +127,26 @@ class Game extends Component {
           </div>
         </div>
         <div className="game-replay">
-          <div className="flex">
-            <div className="game-replay__moments">
-              <ul className="child-borders-y">
-                {this.renderMoments()}
-              </ul>
+          <div className="padding-0-5 relative">
+            {this.renderPlayers()}
+            <Isvg src={rink} className="stroke-white fill-transparent rink"> </Isvg>
+          </div>
+          <div className="game-replay__controls flex align-center">
+            <div className="margin-0-5 margin-right size-0-75">
+              00:00
             </div>
-            <div className="game-replay__player">
-              <Isvg src={rink} className="stroke-white fill-transparent rink"> </Isvg>
+            <div className="grow">
+              <Slider min={0} max={20} defaultValue={3} />
+            </div>
+            <div className="margin-0-5 margin-left size-0-75">
+              {this.state.game.time}
             </div>
           </div>
-          <div className="game-replay__controls">
-            <Slider min={0} max={20} defaultValue={3} />
-          </div>
+        </div>
+        <div className="game-replay__moments">
+          <ul className="child-borders-y">
+            {this.renderMoments()}
+          </ul>
         </div>
         <div className="grow bg-primary-darken-1">
         </div>
